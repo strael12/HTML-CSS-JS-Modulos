5d700a9bb5a95e234b2624a39361b86af645d7be
Rafael Macedo <rafaelmacedosilva88@hotmail.com>
2022-08-03 09:17:28 -0300

    Novas aulas dicionadas

--- HTML+CSS+JS/module-01/aula-01/assets/js/app.js
+++ HTML+CSS+JS/module-01/aula-01/assets/js/app.js
@@ -1,8 +1,121 @@
-const button = document.querySelector('.btn');
+// Função 
+// Sem parametro 
+// function calculaMedia(){
+//     let nota1 = Number(prompt('Digite a primeira nota: '));
+//     let nota2 = Number(prompt('Digite a segunda nota: '));
+//     let nota3 = Number(prompt('Digite a terceira nota: '));
+//     let media = (nota1 + nota2 + nota3) / 3;
+
+//     if (media < 3) {
+//         console.log('REPROVADOOO! Nota Final = ' + media.toFixed(1));
+//     }else if(media > 3 && media < 7){
+//         console.log('Tem jeito Nota Final = ' + media.toFixed(1));
+//     } else if (media >= 7) {
+//         console.log('Aprovadíssimo! Nota Final = ' + media.toFixed(1));
+//     }
+   
+// }
+
+// calculaMedia();
 
 
-button.addEventListener('click', function(){
-    console.log('Ok')
-});  
 
 
+// Filtra média
+// function validaMedia(media){
+//     if (media < 3) {
+//         console.log('REPROVADOOO! Nota Final = ' + media.toFixed(1));
+//     }else if(media > 3 && media < 7){
+//         console.log('Tem jeito Nota Final = ' + media.toFixed(1));
+//     } else if (media >= 7) {
+//         console.log('Aprovadíssimo! Nota Final = ' + media.toFixed(1));
+//     }
+
+//     return media;
+// }
+
+// function cMedia(media){
+//     if (media < 3) {
+//         console.log('REPROVADOOO! Nota Final = ' + media.toFixed(1));
+//     }else if(media > 3 && media < 7){
+//         console.log('Tem jeito Nota Final = ' + media.toFixed(1));
+//     } else if (media >= 7) {
+//         console.log('Aprovadíssimo! Nota Final = ' + media.toFixed(1));
+//     }
+// }
+
+
+
+// Com Parametro 
+// function calculaMedia(n1,n2,n3,media){
+//     n1 = Number(prompt('Digite a primeira nota: '));
+//     n2 = Number(prompt('Digite a segunda nota: '));
+//     n3 = Number(prompt('Digite a terceira nota: '));
+//     media = (n1 + n2 + n3) / 3;
+//     cMedia(media);
+// }
+
+// calculaMedia();
+
+
+
+
+// function calculadora(n1,n2,soma){
+//     n1 = 10;
+//     n2 = 20;
+//     soma = n1 + n2;
+//     console.log(soma);
+// }
+// calculadora();
+
+
+
+
+// let username;
+// username = '432nns';
+// username = '99jssi';
+
+
+// console.log(username);
+
+// function saudacao(nome){
+//     console.log('Bom dia ' + nome);
+// }
+
+// saudacao('Rafael');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Closure
+
+
+// Conceito de callback 
+
+
+// Função arrow 
+
+
+// IIFE --> base de um constructor  
+// (function(){
+//     console.log('ok');
+// }())
